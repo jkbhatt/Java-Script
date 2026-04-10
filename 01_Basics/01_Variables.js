49640b6d2b984e9856b37ca6fb493b7db66847ec
jkbhatt <161799521+jkbhatt@users.noreply.github.com>
2026-04-10 09:00:18 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let account = "Jay@google.com"
+var accountPassword = "12345"
+accountCity = "Amreli"
+
+// accountId = 2
+
+console.log(accountId);
+
+console.table([accountId , accountPassword , accountCity])
